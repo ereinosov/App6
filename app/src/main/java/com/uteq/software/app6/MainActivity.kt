4ab3ff7551e2706beed5837b72a02ec692774f40
ereinosov <ereinosov@uteq.edu.ec>
2026-06-15 15:35:11 -0500

    initial commit

---
+++ app/src/main/java/com/uteq/software/app6/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.uteq.software.app6
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val toolbar =
+            findViewById<Toolbar>(R.id.topAppBar)
+        setSupportActionBar(toolbar);
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_nuevo -> {
+                Toast.makeText(this, "Click en Nuevo", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.search -> {
+                Toast.makeText(this, "Click en Buscar",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "Click en Settings",Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
